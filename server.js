8ac57b6c215f3f78974c991ccb05951843553485
zyrass <zyrass@outlook.fr>
2021-06-07 08:52:48 +0200

    Express content
    Ajout du contenue du fichier server.js, express + début websocket.

---
+++ server.js
@@ -0,0 +1,26 @@
+// Dépendances
+const path    = require("path")
+const express = require("express")
+const ws      = require("ws")
+
+// Application
+const app = express()
+const port = process.env.PORT || 3001
+
+app.get("*", (req, res) => {
+  res.status(200).sendFile(path.join(__dirname, "index.html"))
+})
+
+const server = app.listen(port, () => console.log(`Server démarré sur http://localhost:${port}`))
+
+// Websocket
+const wss = new ws.Server({ server: server })
+
+wss.on("connection", (socket, req) => {
+  socket.send("Bienvenue sur cette révision.");
+  console.log(socket, req)
+})
+
+wss.on("headers", (headers, req) => {
+  console.log(headers)
+})
